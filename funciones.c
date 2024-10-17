743ea3c9d908c70d2d9d2228528c09516715c9f3
carlos.guaita <carlos.guaita@udla.edu.ec>
2024-10-17 10:52:54 -0500

    creacion funcion suma

---
+++ funciones.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "funciones.h"
+#include <math.h>
+
+
+int suma(int num1, int num2) {
+    return num1 + num2;
+}   
--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include "funciones.h"
 
 int main (int argc, char *argv[]) {
 
     int num1=0, num2=0;
-    
+
+    int s= suma(num1,num2);
 
     return 0;
 }
